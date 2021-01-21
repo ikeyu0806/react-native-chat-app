68ea5657a11d2e36de26a6a4c1c02dbc81949def
ikeyu0806 <ikeyu0806@gmail.com>
2021-01-21 16:47:43 +0900

    implement send message view

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>チャット</Text>
+       <TextInput
+        style={styles.sendTextInput}
+        placeholder="メッセージを入力してください。"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40
   },
+  title: {
+    fontSize: 20,
+    marginLeft: 10,
+  },
+  sendTextInput: {
+    marginTop: 10,
+    padding: 10,
+    fontSize: 20,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'gray',
+    justifyContent: 'flex-start',
+    marginLeft: 10,
+  }
 });
