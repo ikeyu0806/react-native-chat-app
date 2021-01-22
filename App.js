71215b4e48d7e27aa32c295f1b7373d47c3fed01
ikeyu0806 <ikeyu0806@gmail.com>
2021-01-22 11:40:29 +0900

    display send message

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const socket = io('http://localhost:4000', {transports: ['websocket']} );
 
 export default function App() {
   const [myMessage, setMyMessage] = useState('')
-  const [sendMessage, setSendMessage] = useState({})
+  const [sendMessages, setsendMessages] = useState({})
   const [messageID, setMessageID] = useState(0)
 
   socket.on('message', function(msg){
@@ -17,26 +17,27 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Chat</Text>
-      <View style={styles.message}>
-        <TextInput
-          style={styles.selfTextInput}
-          placeholder="メッセージを入力してください。"
-          onChange={(e) => {setMyMessage(e.target.value)}}
-          value={myMessage}
-        />
-        <View style={styles.space} />
-      </View>
-      <View style={styles.message}>
-        <View style={styles.space} />
-        <Text
-          style={styles.otherMessage}
-        >相手の送信メッセージ</Text>
-      </View>
+      {Object.entries(sendMessages).map(([key, value]) => (
+        <View style={styles.message} key={key}>
+          <Text
+            style={value.type === 'myMessage' ? styles.selfMessage : styles.otherMessage}
+          >{value.message}</Text>
+        </View>
+      ))}
+      <TextInput
+        style={styles.textInput}
+        placeholder="メッセージを入力してください。"
+        onChange={(e) => {setMyMessage(e.target.value)}}
+        value={myMessage}
+      />
+      <View style={styles.space} />
       <Button
-        title="emit"
+        title="メッセージ送信"
         onPress={() => {
           socket.emit('message', myMessage)
-          setSendMessage(Object.assign(sendMessage, { [messageID]: {message: myMessage, type: 'myMessage'}}))
+          setMessageID(messageID + 1)
+          setsendMessages(Object.assign(sendMessages, { [messageID]: {message: myMessage, type: 'myMessage'}}))
+          console.log(sendMessages)
          }
         }
       />
@@ -59,7 +60,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
   },
-  selfTextInput: {
+  selfMessage: {
     marginTop: 10,
     padding: 10,
     fontSize: 20,
@@ -85,5 +86,15 @@ const styles = StyleSheet.create({
     color: 'white',
     backgroundColor:'blue',
     overflow: 'hidden'
-  }
+  },
+  textInput: {
+    marginTop: 10,
+    padding: 10,
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    flexDirection: 'row',
+    borderRadius: 20,
+    margin: 10
+  },
 });
